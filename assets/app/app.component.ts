02282b3460715f8eae50c6a8b91f3d1ee2c8d25e
spiking <a.thuvesen@gmail.com>
2016-09-18 11:09:20 +0200

    Added message field and basic styles

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,10 +1,49 @@
 import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
-    template: `  
-        <h1>Hello World!</h1>
-    `
+    template: `
+		<div class="row">
+			<section class="col-md-8 col-md-offset-2">
+				<input type="text" [(ngModel)]="message.content">
+			</section>
+		</div>
+  		<div class="row">
+			<section class="col-md-8 col-md-offset-2">
+      			<article class="panel panel-default">
+					<div class="panel-body">
+						{{ message.content }}
+					</div>
+					<footer class="panel-footer">
+						<div class="author">
+							{{ message.author }}
+						</div>
+						<div class="config">
+							<a href="#">Edit</a>
+							<a href="#">Delete</a>
+						</div>
+					</footer>
+				</article>
+			</section>
+		</div>
+    `,
+	styles: [`
+		.author {
+			display: inline-block;
+			font-style: italic;
+			font-size: 12px;
+			width: 80%;
+		}
+		.config {
+			display: inline-block;
+			text-align: right;
+			font-size: 12px;
+			width: 19%;
+		}
+	`]
 })
 export class AppComponent {
-
+	message = {
+		content: 'A Message To You',
+		author: 'Adam'
+	};
 }
